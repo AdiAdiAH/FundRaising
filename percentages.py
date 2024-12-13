221f3ed733df67f6e4eb5d78838af8c8d112ece4
Aditya Ram <adityaram2027ca@gmail.com>
2024-12-13 15:51:21 -0800

    initial code to process percentages

--- percentages.py
+++ percentages.py
@@ -1,4 +1,6 @@
 bills = [21, 14, 12, 18, 3, 31, 29]
+alist = [14, 21]
+blist = [50, 50]
 percentages = [6.84, 15.75, 25.68, 3.59, 4.96, 4.10, 39.08]
 print("Hello, World")
 def validatePercentages(percentageList):
@@ -15,8 +17,50 @@ def convertP(percentageList):
     for i in range(0, len(percentageList)):
         newPercentages.append(int(percentageList[i] * 100))
     return newPercentages
+
+def findGCF(x, y):
+    while x > 0:
+        r = y % x
+        y = x
+        x = r
+    return y
+
+def findBigGCF(list):
+    c = list[0]
+    for i in range(1, len(list)-1):
+        c = findGCF(c, list[i])
+    return c
+
+def LCM(list):
+    lcm = 1
+    for i in list:    
+        lcm = lcm * i // findGCF(lcm, i)
+    return lcm
+
+def findNumBills(m, r):
+    numList = []
+    newS = []
+    s = convertP(r)
+    g = findBigGCF(s)
+    for x in range(0, len(s)):
+        newS.append(s[x] / g)
+    c = LCM(m)
+    for i in range(0, len(m)):
+        n = c*(newS[i]/m[i])
+        numList.append(int(n))
+    return numList
+if __name__ == "__main__":
     
-convertP(percentages)
+    x = validatePercentages(percentages)
+    if x:
+        print("The following values add to 100")
+    else:
+        print("The following values do not add to 100")
+    def print1():
+        print("Aditya Ram")
+    print1()
+    print(findNumBills(alist, blist))
+
 '''
 def findGCF(x, y):
     if x <= y:
@@ -36,29 +80,3 @@ def findGCF(x, y):
                 n -= 1
         print(n)
 '''
-def findGCF(x, y):
-    while x > 0:
-        r = y % x
-        y = x
-        x = r
-    return y
-
-def findBigGCF(list):
-    for i in range(0, len(list))
-    findGCF(list[i], list[i+1])
-         
-#print(findGCF(3084, 1424)) 
-
-findBigGCF(convertP(percentages))  
-
-if __name__ == "__main__":
-    
-    x = validatePercentages(percentages)
-
-    if x:
-        print("The following values add to 100")
-    else:
-        print("The following values do not add to 100")
-    def print1():
-        print("Aditya Ram")
-    print1()
